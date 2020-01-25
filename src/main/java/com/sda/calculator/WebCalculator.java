63ade7065dd5da9bcd15f3075fe71d51ce9c1a72
Daria Supińska <dariasupinska@gmail.com>
2020-01-25 14:45:02 +0100

    "display text"

---
+++ src/main/java/com/sda/calculator/WebCalculator.java
@@ -0,0 +1,16 @@
+package com.sda.calculator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WebCalculator extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Hello darkness, my old friend, why are u here, it's 3pm");
+    }
+}
